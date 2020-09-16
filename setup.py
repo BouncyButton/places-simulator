7daab1e9fed3f56cba7851921649467ad7de3be6
BouncyButton <48158116+BouncyButton@users.noreply.github.com>
2020-09-17 00:20:00 +0200

    grafici utenti non serviti

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='places-simulator',
+    version='1.0.0',
+    packages=[''],
+    url='bouncybutton.github.com',
+    license='MIT Licence',
+    author='berga',
+    author_email='luca.bergamin.3@studenti.unipd.it',
+    description='A SimPy-powered simulator used to model human behaviour visiting pubs, restaurants and other POI.'
+)
